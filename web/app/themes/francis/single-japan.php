cdd0a2ff78a191088cc7b872d0307df4a3620bf5
Francis <hyeongjin.kim@claves.co.jp>
2019-07-31 10:52:51 +0900

    싱글페이지 수정

--- web/app/themes/francis/single-japan.php
+++ web/app/themes/francis/single-japan.php
@@ -18,64 +18,65 @@ if (have_posts()):
                             <div class="single-image-container">
                                 <div class="single-image-container__image" style="background-image: url('<?php echo $japanImageUrl; ?>')"></div>
                                 <div class="single-image-container__title-and-tags">
-                                    <div class="single-image-container__date-and-title">
+                                    <div class="date-and-title">
                                         <h1 class="title"><?php the_title(); ?></h1>
                                         <p class="date">Posted on <?php the_date('Y.n.j'); ?></p>
                                     </div>
-                                    <ul class="single-image-container__tags-container">
+                                    <ul class="tags-container">
                                         <?php
                                         $japanTags = get_the_terms($post->ID, 'japan-tag');
                                         if ($japanTags) {
                                             foreach ($japanTags as $tag) {
-                                                echo '<li class="single-image-container__tags-containerer__tag"><p>#' . esc_html($tag->name) . '</p></li>';
+                                                echo '<li class="tags-container__tag"><p>#' . esc_html($tag->name) . '</p></li>';
                                             }
                                         } ?>
                                     </ul>
                                 </div>
                             </div>
                             <div class="content-container">
-                                <?php the_content(); ?>
-                            </div>
-                        </div>
+                                <div class="content">
+                                    <?php the_content(); ?>
+                                </div>
 
+                                <div class="single-page__pagination">
+                                    <?php
+                                    $previous_post = get_previous_post();
+                                    $next_post = get_next_post();
+                                    ?>
+                                    <div class="previous-page">
+                                        <?php
+                                        if (!empty($next_post)):
+                                            $next_post_id = $next_post->ID; ?>
+                                            <a class="previous-page__link" href="<?= get_the_permalink($next_post_id); ?>">
+                                                <div class="pagination-container">
+                                                    <time class="pagination-date-and-title__date"><?= get_the_time('Y.n.j', $next_post_id); ?></time>
+                                                    <p class="pagination-date-and-title__title"><?= get_the_title($next_post_id); ?></p>
+                                                    <span class="arrow-left"></span>
+                                                </div>
+                                            </a>
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="next-page">
+                                        <?php
+                                        if (!empty($previous_post)):
+                                            $previous_post_id = $previous_post->ID; ?>
+                                            <a class="next-page__link" href="<?= get_the_permalink($previous_post_id); ?>">
+                                                <div class="pagination-container">
+                                                    <time class="pagination-date-and-title__date"><?= get_the_time('Y.n.j', $previous_post_id); ?></time>
+                                                    <p class="pagination-date-and-title__title"><?= get_the_title($previous_post_id); ?></p>
+                                                    <span class="arrow-right"></span>
+                                                </div>
+                                            </a>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
 
-                        <div class="single-page__pagination">
-                            <?php
-                            $previous_post = get_previous_post();
-                            $next_post = get_next_post();
-                            ?>
-                            <div class="previous-page">
-                                <?php
-                                if (!empty($next_post)):
-                                    $next_post_id = $next_post->ID; ?>
-                                    <a class="previous-page__link" href="<?= get_the_permalink($next_post_id); ?>">
-                                        <div class="pagination-container">
-                                            <time class="pagination-date-and-title__date"><?= get_the_time('Y.n.j', $next_post_id); ?></time>
-                                            <p class="pagination-date-and-title__title"><?= get_the_title($next_post_id); ?></p>
-                                            <span class="arrow-left"></span>
-                                        </div>
+                                <div class="link-to-archive">
+                                    <a class="link-to-archive__btn" href="<?= get_post_type_archive_link('japan'); ?>">
+                                        <span class="text">일본이야기 목록</span>
                                     </a>
-                                <?php endif; ?>
+                                </div>
                             </div>
-                            <div class="next-page">
-                                <?php
-                                if (!empty($previous_post)):
-                                    $previous_post_id = $previous_post->ID; ?>
-                                    <a class="next-page__link" href="<?= get_the_permalink($previous_post_id); ?>">
-                                        <div class="pagination-container">
-                                            <time class="pagination-date-and-title__date"><?= get_the_time('Y.n.j', $previous_post_id); ?></time>
-                                            <p class="pagination-date-and-title__title"><?= get_the_title($previous_post_id); ?></p>
-                                            <span class="arrow-right"></span>
-                                        </div>
-                                    </a>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-
-                        <div class="link-to-archive">
-                            <a class="link-to-archive__btn" href="<?= get_post_type_archive_link('japan'); ?>">
-                                <span class="text">일본이야기 목록</span>
-                            </a>
                         </div>
                     </div>
                 </div>
